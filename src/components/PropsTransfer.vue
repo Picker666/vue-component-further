1d50075418edcb15704c7f5b677e73b5e456d7e5
Christine-Only <tsy17321289030@163.com>
2023-06-26 16:45:46 +0800

    props transfer

---
+++ src/components/PropsTransfer.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>{{ serialNumber }}、{{ title }}</h1>
+    <BlogPost v-bind="blogPost" :num="count"/>
+    <button @click="countPlus">count({{ count }}) +1</button>
+  </div>
+</template>
+
+<script>
+
+const BlogPost = {
+  name: 'blogPost',
+  props: {
+    name: String,
+    company: String,
+    num: Number,
+  },
+  computed: {
+    counter() {
+      return this.num * 2;
+    }
+  },
+  template: `<div>
+    <p>{{name}} - {{company}}</p>
+    <p>{{counter}}</p>
+    </div>`
+}
+
+export default {
+  name: "PropsTransfer",
+  props: {
+    serialNumber: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  },
+  components: {
+    BlogPost
+  },
+  data: function () {
+    return {
+      title: "Props Transfer",
+      blogPost: {
+        name: 'Picker blog',
+        company: 'secret...'
+      },
+      count: 0
+    }
+  },
+  methods: {
+    countPlus() {
+      this.count++;
+    }
+  }
+}
+</script>
---
+++ src/components/temp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ serialNumber }}、{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "needName",
+  props: {
+    serialNumber: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  },
+  data: function () {
+    return {
+      title: "",
+    }
+  }
+}
+</script>
